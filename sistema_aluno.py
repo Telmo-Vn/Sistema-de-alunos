2be098b9410dcdddf3d33649c8445ee1d31fc0d5
Telmo <telmo123.vinicius@gmail.com>
2026-09-23 18:55:47 -0300

    Adicionado menu principal

---
+++ sistema_aluno.py
@@ -0,0 +1,7 @@
+while True:
+    print("1 - Cadastro aluno\n2 - Lista alunos\n3 - Buscar aluno\n4 - Ver situação\n5 - Remover aluno\n6 - Sair")
+    escolha = int(input(""))
+    if escolha == 6:
+        break
+    elif escolha > 6 or escolha <1 :
+        print("Escolha invalida. Por favor escolha uma das opçãoes validas")
