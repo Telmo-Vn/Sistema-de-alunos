56d4946af8af66a6848c2aa3e21c48ff1c88b1ee
Telmo <telmo123.vinicius@gmail.com>
2026-09-25 15:47:05 -0300

    Adicionado funcionalidade de cadastro de alunos

--- sistema_aluno.py
+++ sistema_aluno.py
@@ -1,7 +1,6 @@
 
 
-
-
+lista_aluno = []
 while True:
     print("1 - Cadastro aluno\n2 - Lista alunos\n3 - Buscar aluno\n4 - Ver situação\n5 - Remover aluno\n6 - Sair")
     
@@ -16,8 +15,39 @@ while True:
             print("1 - Cadastro aluno\n2 - Lista alunos\n3 - Buscar aluno\n4 - Ver situação\n5 - Remover aluno\n6 - Sair")
     
 
+    match escolha:
+        case 1:
+                while True:
+                    
+                    nome = str(input("Digite o Nome do Aluno: "))
+                    idade = int(input("Digite a Idade do Aluno: "))
+                    nota = float(input("Digite a Nota do Aluno: "))
+                    
+                    resposta = str((input)("Deseja cadastrar mais algun aluno [S/N]: "))
+                    
 
+                    lista_aluno.append({
 
+                            'nome':nome,
+                            'idade':idade,
+                            'nota':nota
+            
+                    })
+ 
+                    
+                    if resposta == 'n' or resposta == 'N':
+                        break
+                    elif resposta == 's' or resposta == "S":
+                        continue
+                    else:
+                        print("Opção invalida")
+                        break
+
+                        
+        case 2:
+            print(lista_aluno)
+            
+                
     if escolha == 6:
         break
     elif escolha > 6 or escolha <1 :
